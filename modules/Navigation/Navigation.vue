7dc0aefe3ae4fe5a87e145525f441677941ed277
Mark Dunbavan <mdunbavan@gmail.com>
2021-07-09 16:44:36 +0100

    render links in header with routing to pages.
    Signed-off-by: Mark Dunbavan <mdunbavan@gmail.com>

--- modules/Navigation/Navigation.vue
+++ modules/Navigation/Navigation.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        {{navList}}
-    </div>
+    <nav>
+        <ul class="flex flex-wrap justify-start items-start">
+            <li v-for="navItem, index in navList" :key="index">
+                <NuxtLink class="mr-8" :to="`${navItem.menuLink}`">{{ navItem.name }}</NuxtLink>
+            </li>
+            <GitHubLogo />
+        </ul>
+    </nav>
 </template>
 <script lang="js" src="./Navigation.js" />
