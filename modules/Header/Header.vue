d2a650bfb18d7e05a681fbee6e58ce022ce0d003
Mark Dunbavan <mdunbavan@gmail.com>
2021-07-09 16:44:05 +0100

    update: added logo and github icon in header along with cms links

--- modules/Header/Header.vue
+++ modules/Header/Header.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <Navigation nav-type="header" :nav-list="globalHeaderMenu" />
-    </div>
+    <header class="header py-8">
+        <div class="flex flex-wrap justify-between items-start container">
+            <Logo />
+            <Navigation nav-type="header" :nav-list="navigation" />
+        </div>
+    </header>
 </template>
 <script lang="js" src="./Header.js" />
