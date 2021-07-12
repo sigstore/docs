80654c9e36688840e8b7e4b51df9c92dbefa1f20
Mark Dunbavan <mdunbavan@gmail.com>
2021-07-12 11:46:37 +0100

    added SEO to the main routed page
    Signed-off-by: Mark Dunbavan <mdunbavan@gmail.com>

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -9,6 +9,7 @@
 <script>
 
 export default {
+
   async asyncData({ $content, params }) {
     const slug = params.slug || "index";
     const page = await $content(slug)
@@ -17,6 +18,72 @@ export default {
     return {
       page
     };
+  },
+  head() {
+      return this.page ? {
+        title: this.page.title,
+        titleTemplate: "%s · " + process.env.VUE_APP_SITENAME,
+        script: [
+          { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+          {
+            json: {
+              "@context": "https://schema.org",
+              "@type": "NewsArticle",
+              mainEntityOfPage: {
+                "@type": "WebPage",
+                "@id": `${process.env.VUE_APP_FRONTEND + this.$route.path}`
+              },
+              headline: `Home`,
+              url: `${process.env.VUE_APP_FRONTEND + this.$route.path}`
+            },
+            type: "application/ld+json"
+          }
+        ],
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { hid: 'description', name: 'description', content: this.page.description },
+          { name: 'format-detection', content: 'telephone=no' },
+          // Twitter Card
+        {
+          name: "twitter:card",
+          content: process.env.VUE_APP_SITENAME
+        },
+        { name: "twitter:title", content: this.page.title },
+        {
+          name: "twitter:description",
+          content: this.page.description
+        },
+        // image must be an absolute path
+        {
+          name: "twitter:image",
+          content: this.page.description
+        },
+        // Facebook OpenGraph
+        { property: "og:title", content: this.page.title },
+        {
+          property: "og:site_name",
+          content: process.env.VUE_APP_SITENAME
+        },
+        { property: "og:type", content: "website" },
+        {
+          property: "og:image",
+          content: ''
+        },
+        {
+          property: "og:description",
+          content:
+            ""
+        }
+        ],
+        link: [
+          { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+          {
+            rel: "canonical",
+            href: `${process.env.VUE_APP_FRONTEND + this.$route.path}`
+          }
+        ]
+      } : null
   }
 };
 </script>
