71aba7ef9ca18015c034543a8c9a06c2e8ab9975
Mark Dunbavan <mdunbavan@gmail.com>
2021-07-12 11:46:02 +0100

    add conatiner class to footer

--- modules/Footer/Footer.vue
+++ modules/Footer/Footer.vue
@@ -1,6 +1,8 @@
 <template>
-    <footer>
-        <Navigation nav-type="footer" :nav-list="navigation" />
+    <footer class="footer py-16">
+        <div class="flex flex-wrap justify-between items-start container">
+            <Navigation nav-type="footer" :nav-list="navigation" />
+        </div>
     </footer>
 </template>
 <script lang="js" src="./Footer.js" />
