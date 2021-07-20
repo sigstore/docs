98d299ed1cee8a8b73bdae310d1f85f0746b42d7
Mark Dunbavan <mdunbavan@gmail.com>
2021-07-20 16:20:03 +0100

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/base',
-    'swiper/css/swiper.css',
+    'swiper/css/swiper.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
