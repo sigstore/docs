868416fa6a77613740a8487c3bb15356f5119251
Mark Dunbavan <mdunbavan@gmail.com>
2021-08-04 10:24:33 +0100

    hotfix for deploy issue (#21)
    * hotfix for deploy issue
    
    Signed-off-by: Mark Dunbavan <mdunbavan@gmail.com>
    
    * change folder dir
    
    Signed-off-by: Mark Dunbavan <mdunbavan@gmail.com>
    
    * remove mouse swiper events
    
    Signed-off-by: Mark Dunbavan <mdunbavan@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/base'
+    '@/assets/css/base',
+    'swiper/swiper.min.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
